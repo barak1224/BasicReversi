5f76098dc3df92df2cbbd94b06e384fa55556788
Barak <barak1224@gmail.com>
2018-01-10 13:25:48 +0200

    Java converting - DONE

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,39 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        GameFlow game = new GameFlow(8, new ConsolePrinter(), 1);
-        game.run();
+//        GameFlow game = new GameFlow(8, new ConsolePrinter(), 1);
+//        game.run();
+        launch(args);
+        GameSettings gameSettings = null;
+        try {
+            gameSettings = IOSettings.read();
+        } catch (Exception e) {
+            System.out.println("PROBLEM...");
+        }
+        if (gameSettings != null) {
+            System.out.println(gameSettings.getSizeBoard());
+            System.out.println(gameSettings.getColorPlayerOne().toString());
+            System.out.println(gameSettings.getColorPlayerTwo());
+            System.out.println(gameSettings.getWhoStarts());
+        }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        try {
+            GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("MenuFXML.fxml"));
+            Scene scene = new Scene(root,400,350);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setTitle("Menu");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/MenuController.java
@@ -0,0 +1,37 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    protected void runGame() {
+
+    }
+
+    @FXML
+    protected void settings() {
+        try {
+            Stage stage = (Stage) settingsButton.getScene().getWindow();
+            GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("SettingsFXML.fxml"));
+            Scene scene = new Scene(root, 400, 500);
+            stage.setScene((scene));
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Failed to open Settings");
+        }
+    }
+}
